2abff77979f1ed9a29cb76a50f426ee74a13c69b
sven-s <sven@sven-s.de>
2014-07-07 18:46:20 +0200

    Initial

--- GameOfLife/Class1.cs
+++ GameOfLife/Class1.cs
@@ -11,10 +11,43 @@ namespace GameOfLife
     public class Class1
     {
         [Test]
-        public void Test()
+        public void JedelebendezelleMitWenigerAlszweiNachbarnStirbt()
         {
-            Assert.AreEqual(1,0);
+
+            var startArray = new bool [,]
+                             {
+                                 {false, false, false, false, false, false, false, false},
+                                 {false, false, false, false, true, false, false, false},
+                                 {false, false, false, true, false, false, false, false},
+                                 {false, false, false, false, false, false, false, false}
+                             };
+
+
+            var actualArray = schritt(startArray);
+
+
+            var expectedArray = new bool[,]
+                             {
+                                 {false, false, false, false, false, false, false, false},
+                                 {false, false, false, false, false, false, false, false},
+                                 {false, false, false, false, false, false, false, false},
+                                 {false, false, false, false, false, false, false, false}
+                             };
+
+            
+            Assert.AreEqual(actualArray, expectedArray);
         }
 
+
+        private object schritt(bool[,] startArray)
+        {
+            return new bool[,]
+                             {
+                                 {false, false, false, false, false, false, false, false},
+                                 {false, false, false, false, false, false, false, false},
+                                 {false, false, false, false, false, false, false, false},
+                                 {false, false, false, false, false, false, false, false}
+                             };
+        }
     }
 }
